684cbbc4b2cf8b24632ba5aebb7c1f367a9af3f8
kaden.marden <Bommerrrr123@gmail.com>
2020-02-11 10:14:41 -0700

    Create term 2

--- term 3/BlackJack.py
+++ term 3/BlackJack.py
@@ -1,2 +1,57 @@
 import random
+class Card(object):
+    RANKS =("A","2","3","4","5","6","7","8","9","10","J","Q","K")
+    SUITS = ("♤","♡","♢","♧")
 
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        rep = str.format("""
+         ----------
+        | {0}        |
+        | {1}        |
+        |          |
+        |         {1}|
+        |         {0}|
+         ----------
+        """,self.rank,self.suit)
+        return rep
+
+class Hand(object):
+    def __init__(self):
+        self.cards = []
+    def __str__(self):
+        if self.cards:
+            rep = " "
+            for card in self.cards:
+                rep += str(card) + ""
+        else:
+            rep = "Empty Handed"
+        return rep
+
+    def clear(self):
+        self. cards = []
+
+    def add(self,card):
+        self.cards.append(card)
+    def give(self, other_hand, card):
+        self.cards.remove(card)
+        other_hand.add(card)
+
+my_hand = Hand()
+your_hand = Hand()
+for i in range(5):
+    card = Card(random.choice(Card.RANKS), random.choice(Card.SUITS))
+    my_hand.add(card)
+
+print(my_hand)
+print(your_hand)
+my_hand.give(your_hand.cards,my_hand[0])
+print(your_hand)
+print(my_hand)
+my_hand.clear()
+your_hand.clear()
+print(your_hand)
+print(my_hand)
